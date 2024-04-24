cd57612a63c0c1f6b9f5ea4aaf04fbe4a86d4e2f
sudheesh <sudheesh.u@netwebindia.com>
2024-04-24 22:35:45 +0530

    bridal menu added

--- instagram_bot/instagram_bot_app/views.py
+++ instagram_bot/instagram_bot_app/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth import login
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from instagram_bot_app.models import UserStatus
+
 
 @csrf_exempt
 def instagram_webhook(request):
@@ -14,17 +16,38 @@ def instagram_webhook(request):
             print("getting call")
             # Parse JSON data from request body
             data = json.loads(request.body)
-            msg = data['entry'][0]['messaging'][0]['message']['text']
-            msg_id = data['entry'][0]['messaging'][0]['message']['mid']
-            sender_id = data['entry'][0]['messaging'][0]['sender']['id']
-            receiver_id = data['entry'][0]['messaging'][0]['recipient']['id']
-            msg_data = {'msg': msg, 'msg_id': msg_id, 'sender_id': sender_id, 'receiver_id': receiver_id}
-            print(msg_data)
-            send_instgaram_message(message=msg_data['msg'], recipient_id=msg_data['sender_id'])
+            entry = data.get('entry', [])
+            for entry_item in entry:
+                messaging = entry_item.get('messaging', [])
+                for message_item in messaging:
+                    sender_id = message_item['sender']['id']
+                    if 'message' in message_item and 'text' in message_item['message']:
+                        # If it's a normal message
+                        msg = message_item['message']['text']
+                        msg_id = message_item['message']['mid']
+                        receiver_id = message_item['recipient']['id']
+                        msg_data = {'msg': msg, 'msg_id': msg_id, 'sender_id': sender_id, 'receiver_id': receiver_id}
+                        print("incoming", msg_data)
+                        # Check if the sender is in the database
+                        send_instgaram_message_start(message=msg_data['msg'], recipient_id=sender_id)
+                    elif 'postback' in message_item:
+                        # If it's a postback
+                        payload = message_item['postback']['payload']
+                        title = message_item['postback']['title']
+                        print("postback", payload, title)
+                        # Process the payload and send the appropriate response
+                        if payload == 'Bridal':
+                            # If it's a request for the main menu, send it
+                            send_Bridal(recipient_id=sender_id)
+                        elif payload.startswith('OPTION_'):
+                            # If it's an option, process it
+                            # process_option(payload, sender_id)
+                            pass
             
             return HttpResponse('Success', status=200)
         except Exception as e:
-            print(e)
+            # Handle any exceptions
+            print("Error:", str(e))
             return HttpResponse('Error', status=500)
 
     elif request.method == 'GET':
@@ -36,7 +59,7 @@ def instagram_webhook(request):
             print("tokens",hub_challenge,hub_verify_token)
             return HttpResponse(hub_challenge)
         
-def send_instgaram_message(message='', recipient_id=''):
+def send_instgaram_message_start(message='', recipient_id=''):
     print("send",message,recipient_id)
     url = 'https://graph.facebook.com/v19.0/'
     page_id = '242698862268160'
@@ -44,32 +67,31 @@ def send_instgaram_message(message='', recipient_id=''):
 
     if message == message:
         response_message = {
-                            "attachment": {
-                                "type": "template",
-                                "payload": {
-                                    "template_type": "button",
-                                    "text": "Please select an option:",
-                                    "buttons": [
-                                        {
-                                            "type": "postback",
-                                            "title": "Option 1",
-                                            "payload": "OPTION_1"
-                                        },
-                                        {
-                                            "type": "postback",
-                                            "title": "Option 2",
-                                            "payload": "OPTION_2"
-                                        },
-                                        {
-                                            "type": "postback",
-                                            "title": "Option 3",
-                                            "payload": "OPTION_3"
-                                        }
-                                    ]
-                                }
-                            }
-                        }
-
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "button",
+            "text": "Welcome to Zara's Makeover. Please select an option:",
+            "buttons": [
+                {
+                    "type": "postback",
+                    "title": "Bridal",
+                    "payload": "Bridal"
+                },
+                {
+                    "type": "postback",
+                    "title": "Hair Dressing",
+                    "payload": "Hair Dressing"
+                },
+                {
+                    "type": "postback",
+                    "title": "Make up",
+                    "payload": "Make up"
+                }
+            ]
+        }
+    }
+}
     # Prepare the request data
     params = {
         'recipient': {'id': recipient_id},
@@ -86,34 +108,54 @@ def send_instgaram_message(message='', recipient_id=''):
         return JsonResponse(response_data)
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
-    #     response_message = '''
-    #         Hello welcome user,
-    #         These are the options you can choose from:
-    #         0 - Main menu
-    #         1 - First option
-    #         2 - Second option
-    #         3 - Third option
-    #     '''
-    # elif message == '1':
-    #     response_message = "Option 1"
-    # elif message == '2':
-    #     response_message = "Option 2"
-    # elif message == '3':
-    #     response_message = "Option 3"
-    # else:
-    #     response_message = "For the main menu, type 0"
+    
 
-    # msg_url = url + page_id + '/messages'
-    # params = {
-    #     'recipient': {'id': recipient_id},
-    #     'message': {'text': response_message},
-    #     'access_token': page_access_token
-    # }
-    # print(msg_url)
-    # try:
-    #     response = requests.post(url=msg_url, json=params)
-    #     response_data = response.json()
-    #     print(response_data)
-    #     return JsonResponse(response_data)
-    # except Exception as e:
-    #     return JsonResponse({'error': str(e)}, status=500)
+def send_Bridal( recipient_id=''):
+    print("send",recipient_id)
+    url = 'https://graph.facebook.com/v19.0/'
+    page_id = '242698862268160'
+    page_access_token = 'EAAGFqWZAZAX0QBOzd2762CYwTh6SZBBw3TZA5nOyPfPkT7RHThNWz5jQQ6HAvyrlRn5FGV79N8TJ4kjuZCFx5BARJHrpUtWmrLpBEx0Mz8tDZA96Kwolllc2cKhl4l4ikDJpsoRazmYqRpTZBk3MzbMbsGu1JGdsZCSEbBSbj53sLu4dUpdO7aL8bvBexH8xKXUS'
+
+    
+    response_message = {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "button",
+            "text": "Please select an option:",
+            "buttons": [
+                {
+                    "type": "postback",
+                    "title": "Full Bridal With Family",
+                    "payload": "Full Bridal With Family"
+                },
+                {
+                    "type": "postback",
+                    "title": "Solo Bridal",
+                    "payload": "Solo Bridal"
+                },
+                {
+                    "type": "postback",
+                    "title": "Couple",
+                    "payload": "Couple"
+                }
+            ]
+        }
+    }
+    }
+    # Prepare the request data
+    params = {
+        'recipient': {'id': recipient_id},
+        'message': response_message,
+        'access_token': page_access_token
+    }
+
+    # Send the message
+    msg_url = url + page_id + '/messages'
+    try:
+        response = requests.post(url=msg_url, json=params)
+        response_data = response.json()
+        print("res",response_data)
+        return JsonResponse(response_data)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
